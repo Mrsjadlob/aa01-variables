b588997d76e26cd379ad9b25df83da361de72ce5
Stephanie <mrsjadlob@gmail.com>
2024-06-05 20:59:30 -0400

    pairing

--- problems/01-variables.js
+++ problems/01-variables.js
@@ -7,35 +7,35 @@ test your work by running your code and the test specs!
 //    as a string:
 
 // Your code here 
-
-
+let firstName = 'Stephanie';
 // 2. Declare a variable called `lastName` and assign it your last name
 //    as a string:
 
 // Your code here 
-
-
+let lastName = 'Lopez';
 // 3. Declare a variable called `age`:
 
 // Your code here 
-
+let age;
 
 // 4. Print out the `firstName`, `lastName`, and `age` variables. What
 //    do you expect them to be when they get printed?
 
 // Your code here 
-
-
+console.log(firstName);
+console.log(lastName);
+console.log(age);
 // 5. Assign the `age` variable to a number:
 
 // Your code here 
-
-
+age = 27;
 // 6. Print out the `firstName`, `lastName`, and `age` variables. What
 //    do you expect them to be when they get printed?
 
 // Your code here 
-
+console.log(firstName);
+console.log(lastName);
+console.log(age);
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 const obj = {};
